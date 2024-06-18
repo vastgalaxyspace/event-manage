881d7af49d15a8d018f0a0a0ae89d4d2e778df95
student <dhirajchavan355@gmail.com>
2024-06-19 00:00:14 +0530

    update router

--- router/event.router.js
+++ router/event.router.js
@@ -3,5 +3,9 @@ const express=require('express');
 const router=express.Router();
 
 router.route('/createevent').post(eventcontroller.createevent);
+router.route('/update/:id').patch(eventcontroller.updateevent);
+router.route('/delete/:id').delete(eventcontroller.deleteevent);
+router.route('/getevent/:id').get(eventcontroller.getevent);
+router.route('/registerevent/:id').post(eventcontroller.registerevent);
 
 module.exports=router;
